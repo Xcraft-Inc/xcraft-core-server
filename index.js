2514103ef7fe061564071e8f4ac9a78f4825cb4a
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-11-11 10:38:46 +0100

    Prevents to start the server two times; the problem was with the bus commander when the commands are searched. It fixes #47.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.fork = function (callbackDone, callbackStdout, callbackStderr) {
   var path     = require ('path');
   var xProcess = require ('xcraft-core-process');
 
-  xProcess.fork (path.join (__dirname, './server.js'),
+  xProcess.fork (path.join (__dirname, './lib/server.js'),
                  [], {silent: true}, callbackDone, callbackStdout, callbackStderr);
 };

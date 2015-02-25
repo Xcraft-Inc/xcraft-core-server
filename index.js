b7d8a267937fe33fbe2591b1872ea9a4ce54f836
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-02-25 19:10:50 +0100

    Prefer detached over pipe for the daemon API.
    By default xServer is detached (but not daemonized).
    It means that the shell will send the shutdown command.

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var moduleName = 'xcraft';
 var path   = require ('path');
 var daemon = require ('xcraft-core-daemon');
 
-module.exports = function (pipe) {
-  return daemon (moduleName, path.join (__dirname, 'lib/server.js'), pipe);
+module.exports = function (detached) {
+  return daemon (moduleName, path.join (__dirname, 'lib/server.js'), detached);
 };

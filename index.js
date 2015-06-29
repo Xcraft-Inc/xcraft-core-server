9281a6fd9925de41623b9db00ec1e5d27a07ed32
Mathieu Schroeter <schroeter@epsitec.ch>
2015-06-29 11:28:56 +0200

    Add a way to enable/disable logs even if the server is attached.

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var moduleName = 'server';
 var path   = require ('path');
 var daemon = require ('xcraft-core-daemon');
 
-module.exports = function (detached) {
-  return daemon (moduleName, path.join (__dirname, 'lib/server.js'), detached);
+module.exports = function (detached, logs) {
+  return daemon (moduleName, path.join (__dirname, 'lib/server.js'), detached, logs);
 };

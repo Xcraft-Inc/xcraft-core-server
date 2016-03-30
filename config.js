82c4ffd2cff9c1f0d6e758de75d5d9acc984dc03
Mathieu Schroeter <schroeter@epsitec.ch>
2016-03-30 16:46:16 +0200

    Add a user modules path property in a new server config file

---
+++ config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Retrieve the inquirer definition for xcraft-core-server
+ */
+module.exports = [{
+  type: 'input',
+  name: 'userModulesPath',
+  message: 'user modules path',
+  default: ''
+}];

2ae5d001dd3b1bdbaad6b8ba54a9600ae3b030db
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-03-31 08:40:31 +0200

    Use one options paramter to pass the options to xServer

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var moduleName = 'server';
 var path   = require ('path');
 var daemon = require ('xcraft-core-daemon');
 
-module.exports = function (detached, logs) {
-  return daemon (moduleName, path.join (__dirname, 'lib/server.js'), detached, logs);
+module.exports = function (options) {
+  return daemon (moduleName, path.join (__dirname, 'lib/server.js'), options.detached, options.logs);
 };

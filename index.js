07cc8b6510dbb51379a38d7683eb3735cb69abcd
Samuel Loup <loup@epsitec.ch>
2016-08-10 14:28:20 +0200

    Permit runAsLib or runAsDeamon

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ var moduleName = 'server';
 var path   = require ('path');
 var daemon = require ('xcraft-core-daemon');
 
-module.exports = function (options) {
-  return daemon (moduleName, path.join (__dirname, 'bin/server'), options.detached, options.logs, options.response);
+exports.runAsDaemon = (options) => {
+  return daemon (
+    moduleName,
+    path.join (__dirname, 'bin/server'),
+    options.detached,
+    options.logs,
+    options.response
+  );
+};
+
+exports.runAsLib = () => {
+  return {start: require ('./lib/server.js')};
 };
